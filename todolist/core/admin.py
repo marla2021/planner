f80492e6bbeacc03f450f3fd74398129ee0b9e22
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-04-08 18:56:12 +0300

    add UserAdmin

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,13 +1,71 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from todolist.core.models import User
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
-    list_display = ("username", "email", "first_name", "last_name" )
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    search_fields = ("email", "first_name", "last_name")
     list_filter = ("is_staff", "is_active", "is_superuser")
-    search_fields = ("emailfirst_name", "last_name", "username")
-    exclude = ("password", )
-    readonly_fields = ["Last login", "Date joined",]
+    readonly_fields = ("last_login", "date_joined")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                )
+            },
+        ),
+        (
+            "Important dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (None, {"fields": ("username", "password1", "password2")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                )
+            },
+        ),
+    )
+
 
