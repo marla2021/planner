c4571d77306b182b10f409ee885da506783adb32
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-04-08 18:58:52 +0300

    change import

--- todolist/core/admin.py
+++ todolist/core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-from core.models import User
+from todolist.core.models import User
 
 
 @admin.register(User)
