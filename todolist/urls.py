98cbf67430997a4ba751ddd8d711696f2845765c
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-04-18 18:16:03 +0300

    add urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('‹oauth_path ›/', include('social_django.urls', namespace='social')),
     path('core/', include("core.urls")),
 ]
