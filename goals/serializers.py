86eee33f05a7ca8618d0899a5e8339f7e7ca54a1
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-04-20 20:19:47 +0300

    add view

--- goals/serializers.py
+++ goals/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from core.serializers import UserSerializer
 from goals.models import GoalCategory
 
 
@@ -9,3 +11,11 @@ class GoalCreateSerializer(serializers.ModelSerializer):
         model = GoalCategory
         read_only_fields = ("id", "created", "updated", "user")
         fields = "__all__"
+
+class GoalCategorySerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = GoalCategory
+        fields = "__all__"
+        read_only_fields = ("id", "created", "updated", "user")
--- goals/urls.py
+++ goals/urls.py
@@ -5,4 +5,6 @@ from goals import views
 
 urlpatterns = [
     path("goal_category/create", views.GoalCategoryCreateView.as_view()),
+    path("goal_category/list", views.GoalCategoryListView.as_view()),
+    path("goal_category/<pk>", views.GoalCategoryView.as_view()),
 ]
