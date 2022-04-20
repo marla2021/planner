e3f2691edb1ded9ae035864c9f74e7a480189b45
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-04-20 20:09:02 +0300

    add goals

---
+++ goals/views.py
@@ -0,0 +1,10 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework import permissions
+
+from goals.models import GoalCategory
+from goals.serializers import GoalCreateSerializer
+
+class GoalCategoryCreateView(CreateAPIView):
+    model = GoalCategory
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = GoalCreateSerializer
