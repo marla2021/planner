479c3a28cdffec7954fd20fd087ea6a357d5693d
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-05-14 20:08:14 +0300

    add urls

--- todolist/urls.py
+++ todolist/urls.py
@@ -30,11 +30,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-    path('‹oauth_path ›', include('social_django.urls', namespace='social')),
-    path('core', include("core.urls")),
-    path("goals", include("goals.urls")),
-    path("boards", include("goals.urls")),
+    path('admin/', admin.site.urls),
+    path('‹oauth_path ›/', include('social_django.urls', namespace='social')),
+    path('core/', include("core.urls")),
+    path("goals/", include("goals.urls")),
+    path("boards/", include("goals.urls")),
     re_path(r'^swagger/$',schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui',
     ),
