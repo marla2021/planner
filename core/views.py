3e1006118d8e0ee13712d820ac5fd02a33f02373
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-05-14 18:55:51 +0300

    add docker

--- core/views.py
+++ core/views.py
@@ -31,7 +31,8 @@ class LoginView(GenericAPIView):
     def post(self, request, *args, **kwargs):
         s: LoginSerializer = self.get_serializer(data=request.data)
         s.is_valid(raise_exception=True)
-        user = s.validated_data["user"]
+        username = s.validated_data["username"]
+        user = User.objects.get(username=username)
         login(request, user=user)
         user_serializer = UserSerializer(instance=user)
         return Response(user_serializer.data)
