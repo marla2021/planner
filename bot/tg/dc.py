3b5f32461b4262d43cba6b26d917dc6a1f9567b5
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-05-16 19:57:57 +0300

    add commit

--- bot/tg/dc.py
+++ bot/tg/dc.py
@@ -1,5 +1,6 @@
+from typing import List, Optional
+
 from pydantic import BaseModel, Field
-from pydantic.utils import Optional
 
 
 class MessageFrom(BaseModel):
@@ -37,7 +38,7 @@ class UpdateObj(BaseModel):
 
 class GetUpdatesResponse(BaseModel):
     ok: bool
-    result: list[UpdateObj]
+    result: List[UpdateObj]
 
 
 class SendMessageResponse(BaseModel):
