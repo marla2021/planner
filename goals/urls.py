ff96f7a1547a1098ecaff7aa890085120ffc594e
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-05-26 20:26:55 +0300

    add urls

--- goals/urls.py
+++ goals/urls.py
@@ -3,19 +3,19 @@ from django.urls import path
 from goals.views import category, goal, comment, board
 
 urlpatterns = [
-    path("goal_category/create", category.GoalCategoryCreateView.as_view()),
-    path("goal_category/list", category.GoalCategoryListView.as_view()),
-    path("goal_category/<pk>", category.GoalCategoryView.as_view()),
+    path("/goal_category/create", category.GoalCategoryCreateView.as_view()),
+    path("/goal_category/list", category.GoalCategoryListView.as_view()),
+    path("/goal_category/<pk>", category.GoalCategoryView.as_view()),
 
-    path("goal/create", goal.GoalCreateView.as_view()),
-    path("goal/list", goal.GoalListView.as_view()),
-    path("goal/<pk>", goal.GoalView.as_view()),
+    path("/goal/create", goal.GoalCreateView.as_view()),
+    path("/goal/list", goal.GoalListView.as_view()),
+    path("/goal/<pk>", goal.GoalView.as_view()),
 
-    path("goal_comment/create", comment.CommentCreateView.as_view()),
-    path("goal_comment/list", comment.CommentListView.as_view()),
-    path("goal_comment/<pk>", comment.CommentView.as_view()),
+    path("/goal_comment/create", comment.CommentCreateView.as_view()),
+    path("/goal_comment/list", comment.CommentListView.as_view()),
+    path("/goal_comment/<pk>", comment.CommentView.as_view()),
 
-    path("board/create", board.BoardCreateView.as_view()),
-    path("board/list", board.BoardListView.as_view()),
-    path("board/<pk>", board.BoardView.as_view()),
+    path("/board/create", board.BoardCreateView.as_view()),
+    path("/board/list", board.BoardListView.as_view()),
+    path("/board/<pk>", board.BoardView.as_view()),
 ]
