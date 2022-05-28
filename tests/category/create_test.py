69bd27175955c4c6812d17948aa7d55a1fbfad21
marla2021 <91145453+marla2021@users.noreply.github.com>
2022-05-28 17:34:23 +0300

    add test

--- tests/category/create_test.py
+++ tests/category/create_test.py
@@ -8,41 +8,23 @@ from goals.models import GoalCategory
 @freeze_time('2022-05-01T13:06:12.461236Z')
 @pytest.mark.django_db
 def test_create(client):
-    assert not GoalCategory.objects.all()
+    category: List[GoalCategory] = GoalCategory.objects.all()
     response = client.post(
-        "/goals/goal/create/",
+        "/goals/goal_category/create/",
         {
-             "id": 1,
-             "user": {
-                    "id": 1,
-                    "username": "mar.la",
-                    "first_name": None,
-                    "last_name": None,
-                    "email": None
-                },
-        "created": '2022-05-01T13:06:12.461236Z',
-        "updated": '2022-05-01T13:06:12.461236Z',
-        "title": "test",
-        "is_deleted": False,
-        "board": 1
+            "title": "TEST1",
+            "board": 1,
         },
         content_type="application/json",
     )
 
 
-    # assert response.status_code == 201
-    assert response.json() == {
-        "id": 1,
-        "user": {
-            "id": 1,
-            "username": "mar.la",
-            "first_name": None,
-            "last_name": None,
-            "email": None
-        },
-        "created": '2022-05-01T13:06:12.461236Z',
-        "updated": '2022-05-01T13:06:12.461236Z',
-        "title": "test",
-        "is_deleted": False,
-        "board": 1
-    }
+    assert response.status_code == 201
+    assert response.data == {
+    "id": category[0].pk,
+    "created": "2022-05-01T13:06:12.461236Z",
+    "updated": "2022-05-01T13:06:12.461236Z",
+    "title": "TEST1",
+    "is_deleted": False,
+    "board": 1
+}
